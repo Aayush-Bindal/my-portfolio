2c02dca87d02faf81784859ccd1c6485b63ccda2
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2026-05-14 13:04:31 +0530

    fix layout title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
   title: {
-    default: "${DATA.name} | Portfolio",
+    default: `${DATA.name} | Portfolio`,
     template: `%s | ${DATA.name}`,
   },
   description:DATA.metaDescription,
